24ac322817cf5bb472084c7266f4d94977bd92b7
Chua <human+github@chuahou.dev>
2020-10-12 18:22:17 +0800

    kiwami-mgr, kiwami-pkgs: move ttf-mscorefonts to dependency of kiwami-pkgs

--- debian/kiwami-mgr/scripts/ttf-mscorefonts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-
-set -e
-check-root root || exit 1
-
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
-apt-get install ttf-mscorefonts-installer -y
--- debian/kiwami-mgr/scripts/ttf-mscorefonts/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-
-set -e
-check-root root || exit 1
-
-apt-get purge ttf-mscorefonts-installer
